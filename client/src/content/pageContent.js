14f7eb195f158b2c99d1ab2496cc2c14d65f3a8e
LinaBell <linabell@mail.com>
2023-01-18 15:44:28 -0600

    Update pageContent.js

--- client/src/content/pageContent.js
+++ client/src/content/pageContent.js
@@ -8,8 +8,6 @@ export const pageContent = {
   },
   HOME: {
     title: "Welcome!",
-    description:
-      "Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction. What are we going to do? We'll be sent to the spice mine of Kessel or smashed into who knows what! Wait a minute, where are you going? The Death Star plans are not in the main computer. Where are those transmissions you intercepted? What have you done with those plans? We intercepted no transmissions. Aaah....This is a consular ship. It's been totally blown away. What? How? Destroyed...by the Empire! The entire starfleet couldn't destroy the whole planet. It'd take a thousand ships with more fire power than I've. There's another ship coming in. Maybe they know what happened. It's an Imperial fighter. It followed us! No.",
   },
   JUKEBOX: {
     title: "Jukebox",
@@ -25,8 +23,6 @@ export const pageContent = {
   },
   REPLACE_SCENE: {
     title: "Replace Scene",
-    description:
-      "Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction. What are we going to do? We'll be sent to the spice mine of Kessel or smashed into who knows what! Wait a minute, where are you going? The Death Star plans are not in the main computer. Where are those transmissions you intercepted? What have you done with those plans? We intercepted no transmissions. Aaah....This is a consular ship. It's been totally blown away. What? How? Destroyed...by the Empire! The entire starfleet couldn't destroy the whole planet. It'd take a thousand ships with more fire power than I've. There's another ship coming in. Maybe they know what happened. It's an Imperial fighter. It followed us! No.",
     requiresUrlSlug: true,
   },
 };
