fa80657a47a2cedd26f788783ec08a3d81eb4412
LinaBell <linabell@mail.com>
2023-01-18 15:36:21 -0600

    Update playlistGenerator.js

--- server/apps/jukebox/generator/playlistGenerator.js
+++ server/apps/jukebox/generator/playlistGenerator.js
@@ -68,7 +68,6 @@ export const addWebhookWithClick = async ({
 
   await droppedAsset.addWebhook({
     isUniqueOnly: false,
-    assetId: droppedAsset.id,
     dataObject,
     description,
     title,
