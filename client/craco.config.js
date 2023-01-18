3b064fef0ab8330b40818d62a53d2a1539820a21
Lina <linabell@mail.com>
2023-01-17 19:37:51 -0800

    remove plugin

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,21 +1,6 @@
 const path = require("path");
-const CracoAlias = require("craco-alias");
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        baseUrl: "./",
-        aliases: {
-          "@components": "./src/components",
-          "@context": "./src/context",
-          "@pages": "./src/pages",
-          "@utils": "./src/utils",
-        },
-      },
-    },
-  ],
   webpack: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
